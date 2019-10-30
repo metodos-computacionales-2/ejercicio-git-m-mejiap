f47b0da7668bf7d4d6c5559dd03ab000bcea52d0
Default user <m.mejiap@uniandes.edu.co>
2019-10-30 21:06:36 +0000

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,3 +6,13 @@ double perimetro(double r);
 double diametro(double r);
 double area(double r);
 double r0;
+
+int main(){
+    cout<<"Inserte radio del circulo: ";
+    cin>>r0;
+    
+    cout<<"El perimetro es:"<<perimetro(r0)<<endl;
+//    cout<<"El area es:"<<area(r0)<<endl;
+//    cout<<"El diametro es:"<<diametro(r0)<<endl;
+    return 0;
+}
