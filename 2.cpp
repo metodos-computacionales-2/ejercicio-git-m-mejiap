6595f78c4ea38aadbb5e82e82ecafc50a20fdca9
Default user <m.mejiap@uniandes.edu.co>
2019-10-30 22:00:44 +0000

    2.cpp

--- 2.cpp
+++ 2.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-boolean prueba(int a, int b, int c);
+bool prueba(int a, int b, int c);
 int a0;
 int b0;
 int c0;
@@ -15,11 +15,18 @@ int main(){
     cout<<"Inserte lado c: ";
     cin>>c0;
     
-    cout<<prueba(a0,b0,c0)<<endl;
+    if(prueba(a0,b0,c0)==True){
+        cout<<"Si."<<endl;
+    }
+    
+    if(prueba(a0,b0,c0)==False){
+        cout<<"No."<<endl;
+    }
+    
     return 0;
 }
 
-boolean prueba(int a, int b, int c){
+bool prueba(int a, int b, int c){
     if((pow(a,2)+pow(b,2))==pow(c,2)){
         return true;
     }
