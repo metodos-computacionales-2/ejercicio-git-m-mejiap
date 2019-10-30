79124f5cd8d6fd8df6ca728228b6c110b4e6d823
Default user <m.mejiap@uniandes.edu.co>
2019-10-30 21:53:41 +0000

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+boolean prueba(int a, int b, int c);
+int a0;
+int b0;
+int c0;
+
+int main(){
+    cout<<"Inserte lado a: ";
+    cin>>a0;
+    cout<<"Inserte lado b: ";
+    cin>>b0;
+    cout<<"Inserte lado c: ";
+    cin>>c0;
+    
+    cout<<prueba(a0,b0,c0)<<endl;
+    return 0;
+}
